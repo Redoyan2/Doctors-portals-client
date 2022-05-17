995e02bfac77eb44ef19c2c16f241e0d4f7682a9
Azizul kabir lavlu <lavlu.bcse@gmail.com>
2022-05-17 19:05:58 +0600

    footer

--- src/Pages/Home/Info/InfoCard.js
+++ src/Pages/Home/Info/InfoCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const InfoCard = ({img, cardTitle, cardDetails, bgClass }) => {
     return (
-        <div class={`card lg:card-side bg-base-100 shadow-xl p-3 m-1 ${bgClass}`}>
+        <div className={`card lg:card-side bg-base-100 shadow-xl p-3 m-1 ${bgClass}`}>
         <figure className='pl-4'><img src={img} alt="Album"/></figure>
-        <div class="card-body text-white ">
-          <h2 class="card-title">{cardTitle}</h2>
+        <div className="card-body text-white ">
+          <h2 className="card-title">{cardTitle}</h2>
           <p>{cardDetails}</p>
         </div>
       </div>
--- src/Pages/Home/Review.js
+++ src/Pages/Home/Review.js
@@ -3,16 +3,16 @@ import React from 'react';
 const Review = ({ review }) => {
 
     return (
-        <div class="card lg:max-w-lg bg-base-100 shadow-xl gap-5">
-            <div class="card-body">
+        <div className="card lg:max-w-lg bg-base-100 shadow-xl gap-5">
+            <div className="card-body">
                 <p>Lorem ipsum dolor sit amet consectetur
                     adipisicing elit. Praesentium quibusdam quidem
                     veniam culpa beatae at obcaecati assumenda aliquid,
                     reiciendis voluptatem.
                 </p>
                 <div className='flex mt-5'>
-                    <div class="avatar">
-                        <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <div className="avatar">
+                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                             <img src={review.img} />
                         </div>
                     </div>
